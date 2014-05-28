b9b41b7bc742f6bbfc5300075892398bf3575718
LittleKey <LittleKeyRain@gmail.com>
2014-05-28 17:52:34 +0000

    add model, and update....

--- Gemfile
+++ Gemfile
@@ -37,3 +37,5 @@ gem 'spring',        group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+# mysql2
+gem 'mysql2', '~> 0.3.16'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,25 @@
 class HomeController < ApplicationController
-  def root
+  def index
     @page_title = 'LittleKey'
+  end
+
+  def create
+    @article = Article.new(article_param)
+
+    @article.save
+    redirect_to '/'
+  end
+
+  def new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @page_title = @article.title
+  end
+
+  private
+  def article_param
+    params.require(:article).permit(:title, :text, :auth, :time)
   end
 end
