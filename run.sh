ac29868e679a7d49d75d7524309fd57db3c562bf
LittleKey <LittleKeyRain@gmail.com>
2014-05-31 18:24:48 +0000

    update run method, and set port by variable

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if [[ -z $1 ]]; then
+    port=3000
+else
+    port=$1
+fi
 
-./getAddr.rb | { read ipAddr; rails server -b $ipAddr ; }
+./getAddr.rb | { read ipAddr; rails server -b $ipAddr -p $port ; }
